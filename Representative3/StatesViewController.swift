a72d33d57da5a3b46aeb7aecf460be483bc60a7e
Sohailbasha <shlbsh94@gmail.com>
2017-09-08 10:57:46 -0400

    displaying cells for each state

--- Representative3/StatesViewController.swift
+++ Representative3/StatesViewController.swift
@@ -12,29 +12,35 @@ class StatesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
-    
-    
-    
+    @IBOutlet var tableView: UITableView!
     
     
 
-    /*
+    
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
+ 
 
+}
+
+extension StatesViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return StateController.states.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "stateCell", for: indexPath)
+        cell.textLabel?.text = StateController.states[indexPath.row]
+        return cell
+    }
 }
